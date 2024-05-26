790dae5d6740b6c42b9a2951705c2dadcf8ddbef
sbx_user1051 <sbx_user1051@169.254.123.101>
2024-05-26 12:15:20 +0000

    Sun May 26 12:15:20 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -3,6 +3,24 @@ from solutions.CHK import checkout_solution
 
 class TestSum():
     def test_sum(self):
-        
-    
+        assert checkout_solution.checkout("A") == 50
+        assert checkout_solution.checkout("B") == 30
+        assert checkout_solution.checkout("C") == 20
+        assert checkout_solution.checkout("D") == 15
+        assert checkout_solution.checkout("ABCD") == 115
+        assert checkout_solution.checkout("AAA") == 130
+        assert checkout_solution.checkout("BB") == 45
+        assert checkout_solution.checkout("AAAABBBCCDD") == 325
+        assert checkout_solution.checkout("") == -1
+        assert checkout_solution.checkout("E") == -1
+        assert checkout_solution.checkout("123") == -1
+
+
+
+
+
+
+
+
+
 

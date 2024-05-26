6fa0d0da7f78807b48a8f609f299c04dd5cbfa77
sbx_user1051 <sbx_user1051@169.254.123.101>
2024-05-26 13:15:14 +0000

    Sun May 26 13:15:14 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,7 +7,11 @@ def checkout(skus):
     
     """
     # Define prices and offers
-    price_table = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10}
+    price_table = {
+        "A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10, "G": 20, "H": 10,
+        "I": 35, "J": 60, "K": 80, "L": 90, "M": 15, "N": 40, "O": 10, "P": 50,
+        "Q": 30, "R": 50, "S": 30, "T": 20, "U": 40, "V": 50, "W": 20, "X": 90,
+        "Y": 10, "Z": 50}
     offers_table = {"A": [(5, 200), (3, 130)], "B": [(2, 45)], "F": [(3, 20)]} 
 
     # Track total cost of basket
@@ -53,3 +57,4 @@ def checkout(skus):
             total_cost += count * price_table[item]
     
     return total_cost
+

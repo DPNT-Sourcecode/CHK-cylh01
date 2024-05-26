aaa658b26774b65f93a4393fa0c778d49fd38e55
sbx_user1051 <sbx_user1051@169.254.123.101>
2024-05-26 11:59:50 +0000

    Sun May 26 11:59:50 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,9 +1,8 @@
-from solutions.HLO import hello_solution
+from solutions.CHK import checkout_solution
 
 
 class TestSum():
     def test_sum(self):
-        assert hello_solution.hello("Craftsman") == "Hello, Craftsman!"
-        assert hello_solution.hello("Mr. X") == "Hello, Mr. X!"
-
+        
     
+

18146d28d787c1f4c2a59644b4b405840e37f81d
sbx_user1051 <sbx_user1051@169.254.123.101>
2024-05-26 13:09:04 +0000

    Sun May 26 13:09:04 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -8,6 +8,11 @@ class TestSum():
         assert checkout_solution.checkout("C") == 20
         assert checkout_solution.checkout("D") == 15
         assert checkout_solution.checkout("E") == 40
+        assert checkout_solution.checkout("F") == 10
+        assert checkout_solution.checkout("FFF") == 20
+        assert checkout_solution.checkout("FFFF") == 30
+        assert checkout_solution.checkout("FFFFF") == 40
+        assert checkout_solution.checkout("FFFFFF") == 40
         assert checkout_solution.checkout("EEB") == 80
         assert checkout_solution.checkout("EEBB") == 110
         assert checkout_solution.checkout("ABCD") == 115
@@ -16,10 +21,11 @@ class TestSum():
         assert checkout_solution.checkout("AAAAAA") == 250
         assert checkout_solution.checkout("AAAAAAA") == 300
         assert checkout_solution.checkout("BB") == 45
-        assert checkout_solution.checkout("AAAABBBCCDDEE") == 375
+        assert checkout_solution.checkout("AAAABBBCCDDEEFFFFF") == 415
         assert checkout_solution.checkout("") == 0
         assert checkout_solution.checkout("123") == -1
-        assert checkout_solution.checkout("F") == -1
+        assert checkout_solution.checkout("G") == -1
+
 
 
 

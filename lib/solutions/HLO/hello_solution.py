f82b14b5b48416510dd9b5ead58c66f96eb111a0
sbx_user1051 <sbx_user1051@169.254.123.101>
2024-05-26 11:53:45 +0000

    Sun May 26 11:53:45 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,7 +3,17 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    return "Hello, World!"
+    """
+    Outputs a personalised greeting
+
+    Parameters:
+    friend_name (str): The name of the friend
+
+    Returns:
+    str: A personalised greeting message
+    """
+    return f"Hello, {friend_name}!"
+
 
 
 

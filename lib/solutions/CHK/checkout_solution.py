f0208e7740bd6d318223bbd7001072a08e5eacf2
sbx_user1051 <sbx_user1051@169.254.123.101>
2024-05-26 12:36:20 +0000

    Sun May 26 12:36:20 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -12,24 +12,31 @@ def checkout(skus):
 
     total_cost = 0
 
-    # Validate input
+    # Validate input and add items to a counter
     sku_count = {}
     for sku in skus:
-        if sku not in price_table:
+        if sku not in price_table or not isinstance(skus, str) or skus == "":
             return -1
         elif sku in sku_count:
             sku_count[sku] += 1
         else:
             sku_count[sku] = 1
     
+    # Calculate the total price by adding the value of each item
     for item, count in sku_count.items():
+        # Checks if the item has a special offer
         if item in offers_table:
             offer_count, offer_price = offers_table[item]
+            # Calculate how many items this offer can be applied to 
             num_offers = count // offer_count 
+            # Calculate number of remaining items after offer has been applied
             remaining_items = count % offer_count
+            # Add it all up
             total_cost += (num_offers * offer_price) + (remaining_items * price_table[item])
         else:
+            # If item does not have a promotion just add its price onto the total
             total_cost += count * price_table[item]
     
     return total_cost
+
 

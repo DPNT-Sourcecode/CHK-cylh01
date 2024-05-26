ee0c6142c6b140588bcf11fb332950583e77e799
sbx_user1051 <sbx_user1051@169.254.123.101>
2024-05-26 13:04:04 +0000

    Sun May 26 13:04:04 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,7 +8,7 @@ def checkout(skus):
     """
     # Define prices
     price_table = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
-    offers_table = {"A": [(5, 200), (3, 130)], "A": (5, 300), "B": (2, 45)} 
+    offers_table = {"A": [(5, 200), (3, 130)], "B": [(2, 45)]} 
 
     total_cost = 0
 
@@ -37,19 +37,22 @@ def checkout(skus):
     for item, count in sku_count.items():
         # Checks if the item has a special offer
         if item in offers_table:
-            for offer_count, offer_price in sorted(offers_table[item], reverse=True)
-            offer_count, offer_price = offers_table[item]
-            # Calculate how many items this offer can be applied to 
-            num_offers = count // offer_count 
-            # Calculate number of remaining items after offer has been applied
-            remaining_items = count % offer_count
-            # Add it all up
-            total_cost += (num_offers * offer_price) + (remaining_items * price_table[item])
+            for offer_count, offer_price in sorted(offers_table[item], reverse=True):
+                if count >= offer_count:
+                    # Calculate how many items this offer can be applied to 
+                    num_offers = count // offer_count 
+                    # Calculate number of remaining items after offer has been applied
+                    remaining_items = count % offer_count
+                    # Add it all up
+                    total_cost += num_offers * offer_price
+                    count = remaining_items
+            total_cost += count * price_table[item]
         else:
             # If item does not have a promotion just add its price onto the total
             total_cost += count * price_table[item]
     
     return total_cost
+
 
 
 
